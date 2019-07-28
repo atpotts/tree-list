e7741a46e2b5d2a145b54d35608a89e81cae36e7
Alistair Potts <alistairtpotts@gmail.com>
2019-07-28 11:49:46 +0100

    clean up nix files - avoid self-dependent nix builds

--- derivation.nix
+++ derivation.nix
@@ -1,9 +1,16 @@
-{haskellPackages, lib}:
-let src = lib.cleanSource (lib.cleanSourceWith {
-        filter=name: type: let baseName = baseNameOf (toString name);
-            in ! (type == "directory" && baseName == "dist");
-        src=./.;
+{ haskellPackages, lib }:
+let
+  stripHash = path:
+    let m = builtins.split "^[a-z0-9]{32}-" (baseNameOf path);
+    in if builtins.length m == 3 then builtins.elemAt m 2 else path;
+  src = with lib;
+    cleanSource (cleanSourceWith {
+      src = ./.;
+      filter = name: type:
+        let baseName = baseNameOf (toString name);
+        in (!(type == "directory" && baseName == "dist"))
+        && (!hasPrefix "." baseName) && (!hasSuffix ".nix" name);
     });
-
-in haskellPackages.callCabal2nix "tree-list" ./. {}
+  srcName = stripHash (baseNameOf src);
+in haskellPackages.callCabal2nix srcName src { }
 
